5e887dea89dc0d987ea99acca37285a23843cc52
kidchaos04 <vinayakrastogi94@gmail.com>
2020-04-30 15:01:59 +0530

    Updates

---
+++ precache-manifest.c1a72824892d57f2c0b52f83dd242978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efa670044506ca63627af6ba7d358cf",
+    "url": "/react-advice-app/index.html"
+  },
+  {
+    "revision": "82be9080fdf8b23c555a",
+    "url": "/react-advice-app/static/css/main.1fdc01ba.chunk.css"
+  },
+  {
+    "revision": "5d175807939fa87668df",
+    "url": "/react-advice-app/static/js/2.cf68b7bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-advice-app/static/js/2.cf68b7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82be9080fdf8b23c555a",
+    "url": "/react-advice-app/static/js/main.3152c1e8.chunk.js"
+  },
+  {
+    "revision": "6ccad7264253e68813d1",
+    "url": "/react-advice-app/static/js/runtime-main.594402a5.js"
+  }
+]);
